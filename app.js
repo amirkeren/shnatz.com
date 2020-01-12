eb9a380762179e8c6e93dae3a55ac8d35c83c02f
Amir Keren <ak091283@gmail.com>
2020-01-12 22:53:30 +0200

    try to build

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import express, { static } from 'express'
-
+const express = require('express')
 const app = express()
-app.use(static('public'))
+
+app.use(express.static('public'))
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
